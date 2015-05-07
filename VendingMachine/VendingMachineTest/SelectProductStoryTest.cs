31b0a85fa8d9fdd6ea438d7d42ff8ed4404488c3
Joe Hershey <joseph.hershey@gmail.com>
2015-05-07 03:34:38 -0400

    Added check in ShouldDispenseProductWithEnoughMoney test for current amount = 0

--- VendingMachine/VendingMachineTest/SelectProductStoryTest.cs
+++ VendingMachine/VendingMachineTest/SelectProductStoryTest.cs
@@ -19,12 +19,14 @@ namespace VendingMachineTest
             Assert.AreEqual(machine.SelectProduct(Product.Cola), Product.Cola);
             Assert.AreEqual(machine.Display, VendingMachine.VendingMachine.DisplayMessages.THANK_YOU);
             Assert.AreEqual(machine.Display, VendingMachine.VendingMachine.DisplayMessages.INSERT_COIN);
+            Assert.AreEqual(machine.CurrentAmount, 0);
 
             machine.InsertCoin(Coin.Quarter);
             machine.InsertCoin(Coin.Quarter);
             Assert.AreEqual(machine.SelectProduct(Product.Chips), Product.Chips);
             Assert.AreEqual(machine.Display, VendingMachine.VendingMachine.DisplayMessages.THANK_YOU);
             Assert.AreEqual(machine.Display, VendingMachine.VendingMachine.DisplayMessages.INSERT_COIN);
+            Assert.AreEqual(machine.CurrentAmount, 0);
 
             machine.InsertCoin(Coin.Quarter);
             machine.InsertCoin(Coin.Quarter);
@@ -32,6 +34,8 @@ namespace VendingMachineTest
             machine.InsertCoin(Coin.Quarter);
             Assert.AreEqual(machine.SelectProduct(Product.Candy), Product.Candy);
             Assert.AreEqual(machine.Display, VendingMachine.VendingMachine.DisplayMessages.THANK_YOU);
+            Assert.AreEqual(machine.CurrentAmount, 0);
+
             Assert.AreEqual(machine.Display, VendingMachine.VendingMachine.DisplayMessages.INSERT_COIN);
 
         }
