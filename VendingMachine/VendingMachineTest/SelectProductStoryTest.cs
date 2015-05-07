e49b21e2ab5f0a66ade2e3d76151e66c75f41c33
Joe Hershey <joseph.hershey@gmail.com>
2015-05-07 02:10:49 -0400

    add other products and assert display message to dispense test, test pass.

--- VendingMachine/VendingMachineTest/SelectProductStoryTest.cs
+++ VendingMachine/VendingMachineTest/SelectProductStoryTest.cs
@@ -6,15 +6,30 @@ namespace VendingMachineTest
     [TestClass]
     public class SelectProductStoryTest
     {
+
         [TestMethod]
         public void ShouldDispenseProductWithEnoughMoney()
         {
             var machine = new VendingMachine.VendingMachine();
+
             machine.InsertCoin(Coin.Quarter);
             machine.InsertCoin(Coin.Quarter);
             machine.InsertCoin(Coin.Quarter);
             machine.InsertCoin(Coin.Quarter);
             Assert.AreEqual(machine.SelectProduct(Product.Cola), Product.Cola);
+            Assert.AreEqual(machine.Display, VendingMachine.VendingMachine.THANK_YOU_DISPLAY);
+
+            machine.InsertCoin(Coin.Quarter);
+            machine.InsertCoin(Coin.Quarter);
+            Assert.AreEqual(machine.SelectProduct(Product.Chips), Product.Chips);
+            Assert.AreEqual(machine.Display, VendingMachine.VendingMachine.THANK_YOU_DISPLAY);
+
+            machine.InsertCoin(Coin.Quarter);
+            machine.InsertCoin(Coin.Quarter);
+            machine.InsertCoin(Coin.Quarter);
+            machine.InsertCoin(Coin.Quarter);
+            Assert.AreEqual(machine.SelectProduct(Product.Candy), Product.Candy);
+            Assert.AreEqual(machine.Display, VendingMachine.VendingMachine.THANK_YOU_DISPLAY);
 
         }
     }
